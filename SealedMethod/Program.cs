25b1b8ba8e8eb8563120d6f8bfd003d7983ad406
RND-HW3 <RND-HW3@RND-HW3>
2025-03-18 15:57:35 +0900

    초기 커밋.

---
+++ SealedMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe() // virtual로 선언한 method()를
+        {
+
+        }
+    }
+
+    class Derived : Base
+    {
+        public sealed override void SealMe()  // 오버라이딩한 method()에서만 sealed 가능
+        {
+            
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+            
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
